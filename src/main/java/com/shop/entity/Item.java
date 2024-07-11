efe9a9e8eddc6a3b08fe76218620205b33af5669
dozagirugi <dozagirugi@gmail.com>
2024-07-11 14:08:56 +0900

    상품 Entity 설계하기

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -1,27 +1,39 @@
 package com.shop.entity;
 
 import com.shop.constant.ItemSellStatus;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "item")
 @ToString
 @Getter
 @Setter
 public class Item {
 
+    @Id
+    @Column(name = "item_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id; //상품코드
 
+    @Column(nullable = false, length = 50)
     private String itemName; //상품명
 
+    @Column(name = "price", nullable = false)
     private int price; //가격
 
+    @Column(nullable = false)
     private int stock; //재고수량
 
+    @Lob
+    @Column(nullable = false)
     private String itemDetail; //상품설명
 
+    @Enumerated(EnumType.STRING)
     private ItemSellStatus itemSellStatus; //상품판매상태
 
     private LocalDateTime registerTime; //등록시간
